53928d33a69db581503f295055b627a28ec406f3
Milo Rastgoo <hi@informagician.com>
2020-02-20 16:45:16 -0500

    migration first revision

--- migrations/20200220162211_create_table.js
+++ migrations/20200220162211_create_table.js
@@ -3,19 +3,27 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('recipe', tbl => {
       tbl.increments()
-      tbl.text('name',64).unique().notNullable()
+      tbl.string('name',64).unique().notNullable()
   })
   .createTable('ingredient', tbl => {
       tbl.increments()
-      tbl.text('name',64).unique().notNullable()
+      tbl.string('name',64).unique().notNullable()
   })
   .createTable('recipe_ingredient', table => {
-      tbl.integer('recipe_id').unsigned().notNullable().references('id').inTable('recipe')
-      tbl.integer('ingredient_id').unsigned().notNullable().references('id').inTable('ingredient')
+      tbl.integer('recipe_id').unsigned().notNullable().references('id').inTable('recipe').onUpdate('CASCADE').onDelete('RESTRICT')
+      tbl.integer('ingredient_id').unsigned().notNullable().references('id').inTable('ingredient').onUpdate('CASCADE').onDelete('RESTRICT')
       tbl.float('qty').notNullable().unsigned()
+  })
+  .createTable('instructions', tbl => {
+      tbl.integer('recipe_id').unsigned().notNullable().references('id').inTable('recipe').onUpdate('CASCADE').onDelete('RESTRICT')
+      tbl.string('step',256).notNullable()
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('instructions')
+  .dropTableIfExists('recipe_ingredient')
+  .dropTableIfExists('ingredient')
+  .dropTableIfExists('recipe')
 };
